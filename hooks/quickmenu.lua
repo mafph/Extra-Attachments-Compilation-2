4bdcde8b06085e69f6ab174510c51f8ee2adb783
mafph <87659032+mafph@users.noreply.github.com>
2026-09-06 21:18:18 +0100

    Update LMG Steelsights version check in quickmenu

--- hooks/quickmenu.lua
+++ hooks/quickmenu.lua
@@ -8,7 +8,7 @@ Hooks:PostHook(MenuMainState, "at_enter", "showthething", function(self, ...)
             local menuN = QuickMenu:new( "WeaponLib not installed.", "This mod requires WeaponLib. You do not have WeaponLib installed.", {[1] = {text = "OK", is_cancel_button = true},[2] = {text = "Go to download", callback = os.execute("cmd /c start " .. weaponliburl), is_cancel_button = false}} )
             menuN:Show()
     end
-    if BLT.Mods:GetModByName("LMG Steelsights") then
+    if BLT.Mods:GetModByName("LMG Steelsights v0.64") then
             -- local menuY = QuickMenu:new( "WeaponLib installed.", "Everything's fine.", {[1] = {text = "Thanks!", is_cancel_button = true}} )
             -- menuY:Show()
     else
